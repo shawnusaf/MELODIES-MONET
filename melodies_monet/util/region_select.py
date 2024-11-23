0432ace3f06f753e3f41dc95c13118c335fe1889
blychs <plichtig@ucar.edu>
2024-11-22 17:02:13 -0700

    Return masked_dataset directly

--- melodies_monet/util/region_select.py
+++ melodies_monet/util/region_select.py
@@ -27,7 +27,7 @@ def create_custom_mask(data, mask_info):
     Returns
     -------
     xr.Dataset
-        mask for input data
+        masked data
     """
     if not isinstance(mask_info, (list, dict)):
         raise TypeError(f"mask_info type={type(mask_info)}, not valid for create_custom_mask.")
@@ -41,10 +41,11 @@ def create_custom_mask(data, mask_info):
             all_regions.append(rpolygon)
         regions = regionmask.Regions(all_regions, abbrevs=abbrevs, name="custom_regions")
     region_mask = regions.mask(data)
-    return region_mask
+    masked_data = data.where(region_mask.notnull())
+    return masked_data
 
 
-def create_predefined_mask(data, name_regiontype):
+def create_predefined_mask(data, name_regiontype, region_name):
     """Creates mask using regionmask.
 
     Parameters
@@ -53,6 +54,9 @@ def create_predefined_mask(data, name_regiontype):
         Dataset with the data that the user wishes to mask.
     name_regiontype : str
         name of regionmask defined regions (e.g., "srex", "giorgi")
+    region_name : str
+        region to mask. If it can be parsed to an integer, that is used.
+        Otherwise, it is assumed to be an abbrev.
 
     Returns
     -------
@@ -61,10 +65,14 @@ def create_predefined_mask(data, name_regiontype):
     """
     regions = literal_eval(f"regionmask.defined_regions.{name_regiontype}")
     region_mask = regions.mask(data)
-    return region_mask
+    try:
+        selected_region = data.where(region_mask == int(region_name))
+    except ValueError:
+        selected_region = data.where(region_mask.cf == region_name)
+    return selected_region
 
 
-def create_shapefile_mask(data, mask_path=None, mask_url=None, **kwargs):
+def create_shapefile_mask(data, mask_path=None, mask_url=None, region_name=None, **kwargs):
     """Creates mask from shapefile using regionmask and geopandas.
 
     Parameters
@@ -77,6 +85,9 @@ def create_shapefile_mask(data, mask_path=None, mask_url=None, **kwargs):
         url to download the shapefiles. Requires pooch.
         If both mask_url and mask_path are provided, this will be
         ignored.
+    region_name : str
+        region to mask. If it can be parsed to an integer, that is used.
+        Otherwise, it is assumed to be an abbrev.
     **kwargs
         Arguments to pass to regionmask.from_geopandas
 
@@ -103,4 +114,8 @@ def create_shapefile_mask(data, mask_path=None, mask_url=None, **kwargs):
     regions = regionmask.from_geopandas(gp.read_file(file), **kwargs)
 
     region_mask = regions.mask(data)
-    return region_mask
+    try:
+        selected_region = data.where(region_mask == int(region_name))
+    except ValueError:
+        selected_region = data.where(region_mask.cf == region_name)
+    return selected_region
