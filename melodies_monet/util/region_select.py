85f91224ad8bdcb66a4caa828dab0653a4a5ef0d
blychs <plichtig@ucar.edu>
2024-12-03 09:55:30 -0700

    test region support

--- melodies_monet/util/region_select.py
+++ melodies_monet/util/region_select.py
@@ -5,8 +5,18 @@ Masking arbitrary regions with regionmask
 import warnings
 from ast import literal_eval
 
-import geopandas as gp
-import regionmask
+import pandas as pd
+
+from melodies_monet.util.tools import get_epa_region_bounds, get_giorgi_region_bounds
+
+try:
+    import geopandas as gp
+except ImportError:
+    gp = None
+try:
+    import regionmask
+except ImportError:
+    regionmask = None
 
 try:
     import pooch
@@ -67,7 +77,7 @@ def create_predefined_mask(data, name_regiontype, region_name):
     region_mask = regions.mask(data)
     try:
         selected_region = data.where(region_mask == int(region_name))
-    except ValueError:
+    except TypeError:
         selected_region = data.where(region_mask.cf == region_name)
     return selected_region
 
@@ -119,3 +129,135 @@ def create_shapefile_mask(data, mask_path=None, mask_url=None, region_name=None,
     except ValueError:
         selected_region = data.where(region_mask.cf == region_name)
     return selected_region
+
+
+def control_custom_mask(data, domain_type, domain_name, domain_info=None, **kwargs):
+    """Parses region information to return the right type of data.
+
+    Parameters
+    ----------
+    data : xr.Dataset
+        data to be masked
+    domain_type : str
+        type of data. Used to decide which function to apply. Should begin with
+        "custom"
+    domain_name : str
+        It is used as the region name, or to read the info.
+    **kwargs:
+        Extra kwargs to pass to regionmask
+
+    Returns
+    -------
+    xr.Dataset
+        masked Dataset
+    """
+    if "custom" not in domain_type:
+        raise ValueError("If regionmask is used, the domain_type should be starting with 'custom'")
+    if "auto_polygon" in domain_type:
+        masked_data = create_custom_mask(data, domain_info[domain_name])
+    elif "defined_region" in domain_type:
+        name_regiontype = domain_info[domain_name]["name_regiontype"]
+        region_name = domain_info[domain_name]["region_name"]
+        masked_data = create_predefined_mask(data, name_regiontype, region_name)
+    elif "custom_file" in domain_type:
+        mask_path = domain_info[domain_name].get("mask_path", None)
+        mask_url = domain_info[domain_name].get("mask_url", None)
+        region_name = domain_info[domain_name].get("region_name", None)
+        masked_data = create_shapefile_mask(data, mask_path, mask_url, region_name, **kwargs)
+    else:
+        raise ValueError(
+            "Could not identify the type of domain. Should be 'auto-polygon',"
+            + " 'defined_region' or 'custom_file'"
+        )
+    return masked_data
+
+
+def create_autoregion(data, domain_type, domain_name, **kwargs):
+    """Selects a region using predefined boundaries.
+
+    Parameters
+    ----------
+    data : xr.Dataset | pd.DataFrame
+        data to be masked
+    domain_type : str
+        type of data. Used to decide which function to apply.
+    domain_name : str
+        This is used as the region name, or to read the info.
+    kwargs
+        if domain_type is auto-region:custom-name_of_domain,
+        this is used to read in the information on the custom
+        box
+
+    Returns
+    -------
+    xr.Dataset | pd.DataFrame
+        Data as it was provided to the function
+    """
+    auto_region_id = domain_type.split(":")[1]
+    if auto_region_id == "epa":
+        bounds = get_epa_region_bounds(acronym=domain_name)
+    elif auto_region_id == "giorgi":
+        bounds = get_giorgi_region_bounds(acronym=domain_name)
+    elif auto_region_id == "custom":
+        bounds = kwargs["domain_box"][auto_region_id.split("-")[1]]
+    else:
+        raise ValueError(
+            "Currently, region selections whithout a domain query have only "
+            "been implemented for Giorgi and EPA regions. You asked for "
+            f"{domain_type!r}. Soon, arbitrary rectangular boxes, US states and "
+            "others will be included."
+        )
+    if isinstance(data, pd.DataFrame):
+        data_all = data.loc[
+            (data["latitude"] >= bounds[0])
+            & (data["longitude"] >= bounds[1])
+            & (data["latitude"] <= bounds[2])
+            & (data["longitude"] <= bounds[3])
+        ]
+    else:
+        data_all = data.where(
+            (data["latitude"] >= bounds[0])
+            & (data["longitude"] >= bounds[1])
+            & (data["latitude"] <= bounds[2])
+            & (data["longitude"] <= bounds[3]),
+            drop=True,
+        )
+    return data_all
+
+
+def select_region(data, domain_type, domain_name, **kwargs):
+    """Selects a region in whichever format it was provided
+
+    Parameters
+    ----------
+    data : xr.Dataset | pd.DataFrame
+        data to be masked
+    domain_type : str
+        type of data. Used to decide which function to apply.
+    domain_name : str
+        This is used as the region name, or to read the info.
+    **kwargs:
+        extra kwargs to pass to the selector, depending on the type of
+        data or region.
+
+    Returns
+    -------
+    xr.Dataset | pd.DataFrame
+        Region selected. The type will be the same as the input data.
+    """
+
+    if domain_type != "all":
+        if domain_type.startswith("auto-region"):
+            data_masked = create_autoregion(data, domain_type, domain_name, **kwargs)
+        elif domain_type.startswith("custom"):
+            if regionmask is None:
+                raise ImportError(
+                    "regionmask is not installed, cannot create 'custom:' type domain."
+                    + " If your domain is a simple box, try using auto-region:custom-domain_name."
+                )
+            data_masked = control_custom_mask(data, domain_name, domain_type, **kwargs)
+
+        else:
+            data_masked = data.query(domain_type + " == " + '"' + domain_name + '"', inplace=True)
+        return data_masked
+    return data
